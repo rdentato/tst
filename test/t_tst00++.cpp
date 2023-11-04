befb1b0ac8e1536ad6a5a00ddb3a8d1b32d55f7c
rdentato <rdentato@gmail.com>
2023-11-04 14:48:31 +0100

    Removed.

--- test/t_tst00++.cpp
+++
@@ -1,8 +0,0 @@
-//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
-//  SPDX-License-Identifier: MIT
-
-#include <iostream>
-#include <tst.h>  // Ensure the tst framework is included
-
-#include "t_tst00.c"
-
