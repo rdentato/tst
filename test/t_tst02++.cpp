4789849eac7116fb71aa502dae36bb8a02334214
rdentato <rdentato@gmail.com>
2023-11-04 14:49:28 +0100

    Removed.

--- test/t_tst02++.cpp
+++
@@ -1,7 +0,0 @@
-//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
-//  SPDX-License-Identifier: MIT
-
-#include <iostream>
-#include <tst.h>
-
-#include <t_tst02.c>
--- test/t_tst03++.cpp
+++
@@ -1,7 +0,0 @@
-//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
-//  SPDX-License-Identifier: MIT
-
-#include <iostream>
-#include <tst.h>
-
-#include "t_tst03.c"
