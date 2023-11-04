fa473e36e6c23274965e4b63f2d268874a9fba1f
rdentato <rdentato@gmail.com>
2023-11-04 13:39:33 +0100

    Cpp version.

---
+++ test/t_tst04++.cpp
@@ -0,0 +1,7 @@
+//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
+//  SPDX-License-Identifier: MIT
+
+#include <iostream>
+#include <tst.h>  // Ensure the tst framework is included
+
+#include "t_tst04.c"
